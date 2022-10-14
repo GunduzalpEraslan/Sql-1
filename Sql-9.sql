2b73ac27fc4ffbca0d7f0fa4bf498dc64ea04d80
GunduzalpEraslan <111523448+GunduzalpEraslan@users.noreply.github.com>
2022-10-14 15:20:14 +0300

    Create Sql-9.sql

---
+++ Sql-9.sql
@@ -0,0 +1,16 @@
+# Sql-9
+
+
+
+SELECT city, country FROM city
+INNER JOIN country ON city.country_id = country.country_id;
+
+
+
+SELECT first_name,last_name FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id;
+
+
+
+SELECT first_name,last_name FROM rental
+INNER JOIN customer ON rental.customer_id = customer.customer_id;
